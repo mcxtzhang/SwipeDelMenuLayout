79e4878a90d2f6c64c5bcd7fe3109e36bd09fe7b
zhangxutong <zhangxutong@imcoming.cn>
2016-10-26 14:57:45 +0800

    LinearLayout使用Demo

--- app/src/main/java/mcxtzhang/swipedelmenu/LinearLayoutDelDemoActivity.java
+++ app/src/main/java/mcxtzhang/swipedelmenu/LinearLayoutDelDemoActivity.java
@@ -2,6 +2,9 @@ package mcxtzhang.swipedelmenu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import mcxtzhang.listswipemenudemo.R;
 
@@ -11,5 +14,33 @@ public class LinearLayoutDelDemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout_del_demo);
+        LinearLayout llContent = (LinearLayout) findViewById(R.id.llContent);
+        llContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LinearLayoutDelDemoActivity.this, "内容区域被点击", Toast.LENGTH_SHORT).show();
+            }
+        });
+        findViewById(R.id.btnDelete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LinearLayoutDelDemoActivity.this, "删除按钮被点击", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        LinearLayout llContent2 = (LinearLayout) findViewById(R.id.llContent);
+        llContent2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LinearLayoutDelDemoActivity.this, "第二个内容区域被点击", Toast.LENGTH_SHORT).show();
+            }
+        });
+        findViewById(R.id.btnDelete2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LinearLayoutDelDemoActivity.this, "第二个删除按钮被点击", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
