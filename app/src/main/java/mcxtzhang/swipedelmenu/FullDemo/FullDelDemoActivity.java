c7b7cbd5f82008e01747e4a96975d594e74c4422
zhangxutong <zhangxutong@imcoming.cn>
2016-11-17 10:51:43 +0800

    完善Demo信息

--- app/src/main/java/mcxtzhang/swipedelmenu/FullDemo/FullDelDemoActivity.java
+++ app/src/main/java/mcxtzhang/swipedelmenu/FullDemo/FullDelDemoActivity.java
@@ -42,29 +42,33 @@ public class FullDelDemoActivity extends Activity {
         mAdapter.setOnDelListener(new FullDelDemoAdapter.onSwipeListener() {
             @Override
             public void onDel(int pos) {
-                Toast.makeText(FullDelDemoActivity.this, "删除:" + pos, Toast.LENGTH_SHORT).show();
-                mDatas.remove(pos);
-                mAdapter.notifyItemRemoved(pos);//推荐用这个
-                //如果删除时，不使用mAdapter.notifyItemRemoved(pos)，则删除没有动画效果，
-                //且如果想让侧滑菜单同时关闭，需要同时调用 ((CstSwipeDelMenu) holder.itemView).quickClose();
-                //mAdapter.notifyDataSetChanged();
+                if (pos > 0 && pos < mDatas.size()) {
+                    Toast.makeText(FullDelDemoActivity.this, "删除:" + pos, Toast.LENGTH_SHORT).show();
+                    mDatas.remove(pos);
+                    mAdapter.notifyItemRemoved(pos);//推荐用这个
+                    //如果删除时，不使用mAdapter.notifyItemRemoved(pos)，则删除没有动画效果，
+                    //且如果想让侧滑菜单同时关闭，需要同时调用 ((CstSwipeDelMenu) holder.itemView).quickClose();
+                    //mAdapter.notifyDataSetChanged();
+                }
             }
 
             @Override
             public void onTop(int pos) {
-                SwipeBean swipeBean = mDatas.get(pos);
-                mDatas.remove(swipeBean);
-                mAdapter.notifyItemInserted(0);
-                mDatas.add(0, swipeBean);
-                mAdapter.notifyItemRemoved(pos + 1);
-                if (mLayoutManager.findFirstVisibleItemPosition() == 0) {
-                    mRv.scrollToPosition(0);
+                if (pos > 0 && pos < mDatas.size()) {
+                    SwipeBean swipeBean = mDatas.get(pos);
+                    mDatas.remove(swipeBean);
+                    mAdapter.notifyItemInserted(0);
+                    mDatas.add(0, swipeBean);
+                    mAdapter.notifyItemRemoved(pos + 1);
+                    if (mLayoutManager.findFirstVisibleItemPosition() == 0) {
+                        mRv.scrollToPosition(0);
+                    }
+                    //notifyItemRangeChanged(0,holder.getAdapterPosition()+1);
                 }
-                //notifyItemRangeChanged(0,holder.getAdapterPosition()+1);
             }
         });
         mRv.setAdapter(mAdapter);
-        mRv.setLayoutManager(mLayoutManager = new GridLayoutManager(this,2));
+        mRv.setLayoutManager(mLayoutManager = new GridLayoutManager(this, 2));
 
         //6 2016 10 21 add , 增加viewChache 的 get()方法，
         // 可以用在：当点击外部空白处时，关闭正在展开的侧滑菜单。我个人觉得意义不大，
---
+++ app/src/main/java/mcxtzhang/swipedelmenu/LauncherActivity.java
@@ -0,0 +1,39 @@
+package mcxtzhang.swipedelmenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import mcxtzhang.listswipemenudemo.R;
+import mcxtzhang.swipedelmenu.FullDemo.FullDelDemoActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        findViewById(R.id.rv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), FullDelDemoActivity.class));
+            }
+        });
+
+        findViewById(R.id.lv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), ListViewDelDemoActivity.class));
+            }
+        });
+
+        findViewById(R.id.ll).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), LinearLayoutDelDemoActivity.class));
+            }
+        });
+    }
+}
